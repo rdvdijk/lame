0c4bccbd7115bd5b8dcc1cfe2ea75ceaf38fd061
Roel van Dijk <roel@rustradio.org>
2013-01-22 22:55:39 +0100

    Attach more set/get functions.

--- lib/lame.rb
+++ lib/lame.rb
@@ -11,19 +11,63 @@ module LAME
   # These `attach_function` declarations are in the order of lame.h:
 
   # initialization
-  attach_function :lame_init,               [],               :pointer
+  attach_function :lame_init, [], :pointer
 
   # global flags
-  attach_function :lame_get_num_channels,   [:pointer],       :int
-  attach_function :lame_set_num_channels,   [:pointer, :int], :int
-  attach_function :lame_get_framesize,      [:pointer],       :int
+  attach_function :lame_set_num_samples,        [:pointer, :ulong], :int
+  attach_function :lame_get_num_samples,        [:pointer],         :ulong
+  attach_function :lame_set_in_samplerate,      [:pointer, :int],   :int
+  attach_function :lame_get_in_samplerate,      [:pointer],         :int
+  attach_function :lame_set_num_channels,       [:pointer, :int],   :int
+  attach_function :lame_get_num_channels,       [:pointer],         :int
+  attach_function :lame_set_scale,              [:pointer, :float], :int
+  attach_function :lame_get_scale,              [:pointer],         :float
+  attach_function :lame_set_scale_left,         [:pointer, :float], :int
+  attach_function :lame_get_scale_left,         [:pointer],         :float
+  attach_function :lame_set_scale_right,        [:pointer, :float], :int
+  attach_function :lame_get_scale_right,        [:pointer],         :float
+  attach_function :lame_set_out_samplerate,     [:pointer, :int],   :int
+  attach_function :lame_get_out_samplerate,     [:pointer],         :int
+  attach_function :lame_set_analysis,           [:pointer, :int],   :int
+  attach_function :lame_get_analysis,           [:pointer],         :int
+  attach_function :lame_set_bWriteVbrTag,       [:pointer, :int],   :int
+  attach_function :lame_get_bWriteVbrTag,       [:pointer],         :int
+  attach_function :lame_set_decode_only,        [:pointer, :int],   :int
+  attach_function :lame_get_decode_only,        [:pointer],         :int
+  attach_function :lame_set_quality,            [:pointer, :int],   :int
+  attach_function :lame_get_quality,            [:pointer],         :int
+  attach_function :lame_set_mode,               [:pointer, :int],   :int
+  attach_function :lame_get_mode,               [:pointer],         :int
+  attach_function :lame_set_force_ms,           [:pointer, :int],   :int
+  attach_function :lame_get_force_ms,           [:pointer],         :int
+  attach_function :lame_set_free_format,        [:pointer, :int],   :int
+  attach_function :lame_get_free_format,        [:pointer],         :int
+  attach_function :lame_set_findReplayGain,     [:pointer, :int],   :int
+  attach_function :lame_get_findReplayGain,     [:pointer],         :int
+  attach_function :lame_set_decode_on_the_fly,  [:pointer, :int],   :int
+  attach_function :lame_get_decode_on_the_fly,  [:pointer],         :int
+  attach_function :lame_set_nogap_total,        [:pointer, :int],   :int
+  attach_function :lame_get_nogap_total,        [:pointer],         :int
+  attach_function :lame_set_nogap_currentindex, [:pointer, :int],   :int
+  attach_function :lame_get_nogap_currentindex, [:pointer],         :int
+  # TODO: Callbacks
+  # attach_function :lame_set_errorf,             [:pointer, :pointer],   :int
+  # attach_function :lame_set_debugf,             [:pointer, :pointer],   :int
+  # attach_function :lame_set_msgf,               [:pointer, :pointer],   :int
+  attach_function :lame_set_brate,              [:pointer, :int],   :int
+  attach_function :lame_get_brate,              [:pointer],         :int
+  attach_function :lame_set_compression_ratio,  [:pointer, :int],   :int
+  attach_function :lame_get_compression_ratio,  [:pointer],         :int
+  attach_function :lame_set_preset,             [:pointer, :int],   :int
+  attach_function :lame_set_asm_optimizations,  [:pointer, :int, :int], :int
+
+  attach_function :lame_get_framesize,        [:pointer],         :int
 
   # initializing parameters
-  attach_function :lame_init_params,        [:pointer], :int
+  attach_function :lame_init_params, [:pointer], :int
 
   # encoding
   attach_function :lame_encode_buffer,      [:pointer, :pointer, :pointer, :int, :pointer, :int], :int
-
   # UNTESTED!
   attach_function :lame_encode_buffer_int,  [:pointer, :pointer, :pointer, :int, :pointer, :int], :int
 
--- spec/ffi_spec.rb
+++ spec/ffi_spec.rb
@@ -10,6 +10,24 @@ module LAME
 
     context "global flags" do
 
+      it "gets the number of samples" do
+        LAME.lame_get_num_samples(@flags_pointer).should eql 2**32-1
+      end
+
+      it "sets the number of samples" do
+        LAME.lame_set_num_samples(@flags_pointer, 1).should eql 0
+        LAME.lame_get_num_samples(@flags_pointer).should eql 1
+      end
+
+      it "gets the input samplerate" do
+        LAME.lame_get_in_samplerate(@flags_pointer).should eql 44100
+      end
+
+      it "sets the input samplerate" do
+        LAME.lame_set_in_samplerate(@flags_pointer, 22050).should eql 0
+        LAME.lame_get_in_samplerate(@flags_pointer).should eql 22050
+      end
+
       it "gets the number of channels" do
         LAME.lame_get_num_channels(@flags_pointer).should eql 2
       end
@@ -17,7 +35,172 @@ module LAME
       it "sets the number of channels" do
         LAME.lame_set_num_channels(@flags_pointer, 1).should eql 0
         LAME.lame_get_num_channels(@flags_pointer).should eql 1
-        LAME.lame_set_num_channels(@flags_pointer, 2) # restore default. needed?
+      end
+
+      it "gets the scale" do
+        LAME.lame_get_scale(@flags_pointer).should eql 1.0
+      end
+
+      it "sets the scale" do
+        LAME.lame_set_scale(@flags_pointer, 2.0).should eql 0
+        LAME.lame_get_scale(@flags_pointer).should eql 2.0
+      end
+
+      it "gets the scale_left" do
+        LAME.lame_get_scale_left(@flags_pointer).should eql 1.0
+      end
+
+      it "sets the scale_left" do
+        LAME.lame_set_scale_left(@flags_pointer, 2.0).should eql 0
+        LAME.lame_get_scale_left(@flags_pointer).should eql 2.0
+      end
+
+      it "gets the scale_right" do
+        LAME.lame_get_scale_right(@flags_pointer).should eql 1.0
+      end
+
+      it "sets the scale_right" do
+        LAME.lame_set_scale_right(@flags_pointer, 2.0).should eql 0
+        LAME.lame_get_scale_right(@flags_pointer).should eql 2.0
+      end
+
+      it "gets the output samplerate" do
+        LAME.lame_get_out_samplerate(@flags_pointer).should eql 0
+      end
+
+      it "sets the output samplerate" do
+        LAME.lame_set_out_samplerate(@flags_pointer, 48000).should eql 0
+        LAME.lame_get_out_samplerate(@flags_pointer).should eql 48000
+      end
+
+      it "gets the analysis" do
+        LAME.lame_get_analysis(@flags_pointer).should eql 0
+      end
+
+      it "sets the analysis" do
+        LAME.lame_set_analysis(@flags_pointer, 1).should eql 0
+        LAME.lame_get_analysis(@flags_pointer).should eql 1
+      end
+
+      it "gets bWriteVbrTag" do
+        LAME.lame_get_bWriteVbrTag(@flags_pointer).should eql 1
+      end
+
+      it "sets bWriteVbrTag" do
+        LAME.lame_set_bWriteVbrTag(@flags_pointer, 0).should eql 0
+        LAME.lame_get_bWriteVbrTag(@flags_pointer).should eql 0
+      end
+
+      it "gets decode only" do
+        LAME.lame_get_decode_only(@flags_pointer).should eql 0
+      end
+
+      it "sets decode only" do
+        LAME.lame_set_decode_only(@flags_pointer, 0).should eql 0
+        LAME.lame_get_decode_only(@flags_pointer).should eql 0
+      end
+
+      it "gets the quality" do
+        LAME.lame_get_quality(@flags_pointer).should eql -1
+      end
+
+      it "sets the quality" do
+        LAME.lame_set_quality(@flags_pointer, 1).should eql 0
+        LAME.lame_get_quality(@flags_pointer).should eql 1
+      end
+
+      it "gets the mode" do
+        LAME.lame_get_mode(@flags_pointer).should eql 4
+      end
+
+      it "sets the mode" do
+        LAME.lame_set_mode(@flags_pointer, 1).should eql 0
+        LAME.lame_get_mode(@flags_pointer).should eql 1
+      end
+
+      it "gets force ms" do
+        LAME.lame_get_force_ms(@flags_pointer).should eql 0
+      end
+
+      it "sets force ms" do
+        LAME.lame_set_force_ms(@flags_pointer, 1).should eql 0
+        LAME.lame_get_force_ms(@flags_pointer).should eql 1
+      end
+
+      it "gets free format" do
+        LAME.lame_get_free_format(@flags_pointer).should eql 0
+      end
+
+      it "sets free format" do
+        LAME.lame_set_free_format(@flags_pointer, 1).should eql 0
+        LAME.lame_get_free_format(@flags_pointer).should eql 1
+      end
+
+      it "gets find replay gain" do
+        LAME.lame_get_findReplayGain(@flags_pointer).should eql 0
+      end
+
+      it "sets find replay gain" do
+        LAME.lame_set_findReplayGain(@flags_pointer, 1).should eql 0
+        LAME.lame_get_findReplayGain(@flags_pointer).should eql 1
+      end
+
+      it "gets decode on the fly" do
+        LAME.lame_get_decode_on_the_fly(@flags_pointer).should eql 0
+      end
+
+      it "sets decode on the fly" do
+        LAME.lame_set_decode_on_the_fly(@flags_pointer, 1).should eql 0
+        LAME.lame_get_decode_on_the_fly(@flags_pointer).should eql 1
+      end
+
+      it "gets nogap total" do
+        LAME.lame_get_nogap_total(@flags_pointer).should eql 0
+      end
+
+      it "sets nogap total" do
+        LAME.lame_set_nogap_total(@flags_pointer, 1).should eql 0
+        LAME.lame_get_nogap_total(@flags_pointer).should eql 1
+      end
+
+      it "gets nogap current index" do
+        LAME.lame_get_nogap_currentindex(@flags_pointer).should eql 0
+      end
+
+      it "sets nogap current index" do
+        LAME.lame_set_nogap_currentindex(@flags_pointer, 1).should eql 0
+        LAME.lame_get_nogap_currentindex(@flags_pointer).should eql 1
+      end
+
+      # TODO:
+      it "sets errorf"
+      it "sets debugf"
+      it "sets msgf"
+
+      it "gets the brate" do
+        LAME.lame_get_brate(@flags_pointer).should eql 0
+      end
+
+      it "sets the brate" do
+        LAME.lame_set_brate(@flags_pointer, 1).should eql 0
+        LAME.lame_get_brate(@flags_pointer).should eql 1
+      end
+
+      it "gets the compression ratio" do
+        LAME.lame_get_compression_ratio(@flags_pointer).should eql 1
+      end
+
+      it "sets the compression ratio" do
+        LAME.lame_set_compression_ratio(@flags_pointer, 1).should eql 0
+        LAME.lame_get_compression_ratio(@flags_pointer).should eql 1
+      end
+
+      it "sets the preset" do
+        LAME.lame_set_preset(@flags_pointer, 500).should eql 500
+      end
+
+      it "sets asm optimizations" do
+        LAME.lame_set_asm_optimizations(@flags_pointer, 1, 2).should eql 1
       end
 
     end
