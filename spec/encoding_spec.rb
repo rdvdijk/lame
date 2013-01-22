521fa06270a45aa456166482c4e4661c7f6e91d9
Roel van Dijk <roel@rustradio.org>
2013-01-22 22:16:13 +0100

    Fix final encoding bugs (thanks @wmeissner!)

--- spec/encoding_spec.rb
+++ spec/encoding_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 require 'wavefile'
-require 'pry'
 
 describe "Encoding" do
 
@@ -16,8 +15,13 @@ describe "Encoding" do
     # number of samples to read
     framesize = LAME.lame_get_framesize(flags_pointer)
 
+    # input buffers
     left_buffer  = FFI::MemoryPointer.new(:short, framesize)
     right_buffer = FFI::MemoryPointer.new(:short, framesize)
+
+    # output buffer
+    buffer_size = (128*1024)+16384
+    buffer = FFI::MemoryPointer.new(:uchar, buffer_size)
 
     File.open(mp3_path, "wb") do |file|
       reader.each_buffer(framesize) do |read_buffer|
@@ -30,36 +34,24 @@ describe "Encoding" do
         end
         input_buffer_size = read_buffer.samples.size
 
-        puts "read #{input_buffer_size} wav samples"
-
-        # encode
-        buffer_size = (128*1024)+16384
-        buffer = FFI::MemoryPointer.new(:uchar, buffer_size)
-
+        # encode to mp3
         size = LAME.lame_encode_buffer(
           flags_pointer,
           left_buffer, right_buffer, input_buffer_size,
           buffer, buffer_size
         )
 
-        puts "encoded #{size} mp3 bytes"
-
         # write to file
-        (0..size).each do |i|
-          file.print buffer.get_uchar(i).chr
-        end
+        file.write buffer.get_bytes(0, size)
       end
 
       # wrap up
-      buffer_size = (128*1024)+16384
-      buffer = FFI::MemoryPointer.new(:uchar, buffer_size)
-
       size = LAME.lame_encode_flush(flags_pointer, buffer, buffer_size)
-      file.write (0..size).map { |i| buffer.get_uchar(i) }.pack("C*")
+      file.write buffer.get_bytes(0, size)
     end
 
     # close
-    LAME.lame_close(flags_pointer).should eql 0
+    LAME.lame_close(flags_pointer)
   end
 
 end
